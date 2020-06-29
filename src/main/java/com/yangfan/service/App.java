8254ab5087fc2d8b03e3a9db522c80692a95be5b
yangfan <2585928225@qq.com>
2020-06-29 23:24:55 +0800

    Initial commit

---
+++ src/main/java/com/yangfan/service/App.java
@@ -0,0 +1,16 @@
+package com.yangfan.service;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.IOException;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("consumer.xml");
+        context.start();
+        ProviderService providerService = (ProviderService) context.getBean("providerService");
+        String str = providerService.SayHello("hello");
+        System.out.println(str);
+        System.in.read();
+    }
+}
